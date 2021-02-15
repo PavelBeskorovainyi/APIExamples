56ee30096475a6994574b416569a3196c83bedd1
PavelBeskorovainyi <beskor1993@icloud.com>
2021-02-15 12:12:22 +0200

    add removing keyboard observers

--- ExchangeTableViewTest/ExchangeTableViewTest/Weather/MOYA TEST/UI/WeatherViewController.swift
+++ ExchangeTableViewTest/ExchangeTableViewTest/Weather/MOYA TEST/UI/WeatherViewController.swift
@@ -66,6 +66,9 @@ class WeatherViewController: UIViewController {
         self.podlozhka.isHidden = true
     }
     
+    deinit {
+        removeKeyboardNotifications()
+    }
     
 }
 
@@ -179,5 +182,10 @@ private func registerKeyboardNotifications () {
     }
     @objc private func keyboardWillShow () {
         self.podlozhka.isHidden = false
+    }
+    
+    private func removeKeyboardNotifications () {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
 }
